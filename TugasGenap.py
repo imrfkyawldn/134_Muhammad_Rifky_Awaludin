6eec1249ad5b9d927594f4c1fc80a99ad187e4d5
imrfkyawldn <143861209+imrfkyawldn@users.noreply.github.com>
2023-09-09 09:11:52 +0700

    Add files via upload

---
+++ TugasGenap.py
@@ -0,0 +1,30 @@
+# soal 1
+# balok 
+# operasi perhitungan mencari volume balok
+
+p = int(input("inputkan panjang: "))
+l = int(input("inputkan lebar: "))
+t = int(input("inputkan tinggi: "))
+hasil= p*l*t
+
+print("jumlah volume balok ini adalah", hasil)
+
+# soal 2
+# tabung 
+
+phi = 3.14
+r = int(input("masukkan jari jari = "))
+t = int(input("masukkan tinggi = "))
+hasil = phi*r*r*t
+
+print("jumlah volume tabung", hasil)
+
+# soal 3
+# bola
+
+print("operasi mencari volume bola")
+r = int(input("jari jari = "))
+v = 4/3
+volume = 4/3*22/7*r*r*r
+print("hasil dari = ", volume)
+
